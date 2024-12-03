3d7021aeacb47fface813a3c9de96f4a01432f10
Rodrigo Oliveira <rodrigo.silva.oliveira369@gmail.com>
2024-12-02 21:47:49 -0300

    Add Tailwind CSS integration and global styles

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        pixel: "Pixelify Sans",
+      },
+      colors: {
+        pixel: {
+          white: "#ffffff",
+          yellow: "#faab19",
+          orange: "#f58f1e",
+          teal: "#0fa29e",
+          blue: "#026e95",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
