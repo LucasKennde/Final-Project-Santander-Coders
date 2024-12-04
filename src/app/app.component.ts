70b01acfae41dfe6ede51c9852accf30f5b31507
Lucas Kennde <lucaskennde@gmail.com>
2024-12-04 13:22:41 -0300

    add page signup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./componentes/header/header.component";
 import { FooterComponent } from "./componentes/footer/footer.component";
 import { NgStyle } from '@angular/common';
+import { HeaderComponent } from "./componentes/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, NgStyle],
+  imports: [RouterOutlet, FooterComponent, NgStyle, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
